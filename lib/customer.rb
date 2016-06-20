8be6cd52b8bea964d3fd4662aadc9640ff6e7f5b
Cortor <erwan.barbe@gmail.com>
2016-06-20 17:22:18 +0200

    Customer methods added

--- lib/customer.rb
+++ lib/customer.rb
@@ -8,8 +8,29 @@ class Customer
    @@customers
   end
 
-  def initialize (name)
-    @name = name
-    @@customers << self
+  def initialize (options = {})
+    @name = options[:name]
+    add_to_customers
   end
+
+  def self.find_by_name(name)
+    @@customers.find {|customer| customer.name == name}
+    #@@products.each do |item|
+    #  if item.title == title
+    #    item.title
+    #  end
+    # end
+  end
+
+  private
+
+  def add_to_customers
+   @@customers.each do |item|
+      if item.name == @name
+        raise DuplicateProductError, "#{name} already exists"
+       end
+     end
+   @@customers << self
+  end
+
 end
